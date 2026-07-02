c4a8957a4bb16a387c0984f7bc11236b883e0f97
cybershivam <raynavin20@gmail.com>
2026-07-03 02:08:14 +0530

    JAVA SCRIPT

---
+++ LOOP/Count_string.js
@@ -0,0 +1,11 @@
+let a ="shivam"
+for (let i = 0; i <= 0; i++) {
+    
+    console.log("print the name",a[0])
+     console.log("print the name",a[1])
+      console.log("print the name",a[2])
+       console.log("print the name",a[3])
+        console.log("print the name",a[4])
+         console.log("print the name",a[5])
+}
+console.log(a.concat(a[0],a[1],a[2],a[3],a[4],a[5]))
